c038cad6a7d7fa7a18b0f85c114aff6bc0531b87
Jobin John K <jobinjohnk5@gmail.com>
2022-07-20 15:08:06 +0530

    add all remaining sections

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,8 @@ import expressLogo from "./images/express-min.png";
 import eclerx from "./images/eclerx-min.png";
 import IconLink from "./components/IconLink";
 import LogoAndName from "./components/LogoAndName";
+import LPU from "./images/lpu-min.png";
+import uoc from "./images/uoc.png"
 import { v4 as uuidv4 } from "uuid";
 function App() {
   const skills = [
@@ -18,6 +20,17 @@ function App() {
     ["Git", GitLogo],
     ["JavaScript", JSLogo],
     ["HTML & CSS", htmlNcssLogo],
+  ];
+  const projects = [
+    [
+      "GetItDone ",
+      "https://github.com/jobin365/GetItDone",
+      "https://sleepy-ridge-02151.herokuapp.com/",
+      [
+        "A todo list application that allows you to maintain multiple lists of tasks",
+        "Created using React, MongoDB and Express",
+      ],
+    ],
   ];
   return (
     <div className="App">
@@ -75,6 +88,47 @@ function App() {
       </h1>
       <h2>Intern</h2>
       <LogoAndName name="eClerx" logoURL={eclerx} />
+      <p>June 2022 - Present</p>
+      <ul className="justifyText">
+        <li>
+          Currently undergoing training in emailops and salesforce marketing
+          cloud
+        </li>
+      </ul>
+
+      <h1 className="heading">
+        <i className="fas fa-code"></i> Projects
+      </h1>
+      {projects.map((item) => {
+        const [name, gitHubURL, hostedURL, description] = item;
+        return (
+          <div key={uuidv4()}>
+            <h2>
+              {name}
+              <IconLink URL={gitHubURL} icon={"fab fa-github fa-lg githubBlack"} />
+              <IconLink
+                URL={hostedURL}
+                icon={"fas fa-external-link-alt link-blue"}
+              />
+            </h2>
+            <ul className="justifyText">
+              {description.map((item) => {
+                return <li key={uuidv4()}>{item}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
+
+      <h1 className="heading">
+        <i className="fas fa-graduation-cap"></i> Education
+      </h1>
+      <h2>Master of Computer Applications</h2>
+      <LogoAndName name="Lovely Professional University" logoURL={LPU}/>
+      <p>2021 - 2023</p>
+      <h2>Bachelor of Science in Computer Science</h2>
+      <LogoAndName name="University of Calicut" logoURL={uoc}/>
+      <p>2018 - 2021</p>
     </div>
   );
 }
